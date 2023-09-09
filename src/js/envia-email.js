262f3343b90eed898b3a52810963feb110666bd3
Filipe Silva <81836461+filipe-csilva@users.noreply.github.com>
2023-09-09 00:49:33 -0300

    atualizacao

---
+++ src/js/envia-email.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const formulario = document.getElementById("meuFormulario");
+    const resultado = document.getElementById("resultado");
+    const botaoLimpar = document.querySelector("input[type='reset']");
+
+    formulario.addEventListener("submit", function (e) {
+        e.preventDefault();
+
+        const nome = document.getElementById("nome").value;
+        const email = document.getElementById("email").value;
+        const mensagem = document.getElementById("mensagem").value;
+
+        // Você pode fazer validações aqui, se necessário
+
+        // Envie os dados para o servidor
+        const xhr = new XMLHttpRequest();
+        xhr.open("POST", "https://jacobethan-seesaw.000webhostapp.com/envia-email.php", true);
+        xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+        xhr.onreadystatechange = function () {
+                    alert ("E-mail enviado com sucesso!");
+                    botaoLimpar.click();
+        };
+        xhr.send(`nome=${nome}&email=${email}&mensagem=${mensagem}`);
+        
+       
+       
+    });
+});
